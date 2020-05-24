704ded4a4411a1eed62dee7e1ec7b85a5bbeb1c4
Luis Marques <luisserafim99@gmail.com>
2020-05-24 19:11:19 +0100

    General update

--- Introduction/L2-load_time_series_data.py
+++ Introduction/L2-load_time_series_data.py
@@ -2,20 +2,19 @@ import os
 from pandas import read_csv
 from matplotlib import pyplot
 
-series = read_csv(os.path.join(
-    'Introduction', 'daily-births.csv'), header=0, index_col=0)
+series = read_csv('daily-births.csv')
 
 # pandas .head() default lines are 5, but it can be passed as an argument [.head(10)]
-print(f'\nHead: {series.head()}')
+print(f'\nHead:\n{series.head()}')
 
 print(f'\nSize: {series.size}')
 
 # Query
 query = series.query('Date == "1959-01-15"')
-print(f'\nQuery: {query}')
+print(f'\nQuery:\n{query}')
 
 # Summary statistics
-print(f'\nSummary statistics: {series.describe()}')
+print(f'\nSummary statistics:\n{series.describe()}')
 
 # Show graph
 # pyplot.plot(series)

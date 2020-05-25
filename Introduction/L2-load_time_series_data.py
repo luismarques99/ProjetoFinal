a0a0993787777c943b4a481715391aa972340642
Luis Marques <luisserafim99@gmail.com>
2020-05-25 04:04:36 +0100

    Lesson 2 update

--- Introduction/L2-load_time_series_data.py
+++ Introduction/L2-load_time_series_data.py
@@ -2,7 +2,7 @@ import os
 from pandas import read_csv
 from matplotlib import pyplot
 
-series = read_csv(os.path.join('files', 'daily-births.csv'), header=0, index_col=0, parse_dates=True, squeeze=True)
+series = read_csv(os.path.join('files', 'daily-births.csv'), header=0, index_col=0, parse_dates=True)
 
 # pandas .head() default lines are 5, but it can be passed as an argument [.head(10)]
 print(f'\nHead:\n{series.head()}')

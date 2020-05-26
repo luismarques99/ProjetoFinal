1b7a4f3f0639129a25f28ab0a242a5715946cb7c
Luis Marques <luisserafim99@gmail.com>
2020-05-26 17:56:53 +0100

    Semana 1 update

---
+++ semana-1_Introduction/1.Basic-Plots/L3-data_visualization.py
@@ -0,0 +1,49 @@
+import os
+import re
+from pandas import read_csv
+from pandas import DataFrame
+from pandas import Grouper
+from matplotlib import pyplot
+
+series = read_csv(os.path.join("files", "shampoo-sales.csv"), header=0, index_col=0, parse_dates=True, squeeze=True)
+series_b = read_csv(os.path.join("files", "daily-minimum-temperatures.csv"), header=0, index_col=0, parse_dates=True,
+                  squeeze=True)
+
+
+# # Line plots
+# # Line plot
+# series.plot()p
+# # Black dots plot
+# series.plot(style="k.")
+# # Dashed line plot
+# series.plot(style="k-")
+
+
+# # Histogram and density plots
+# # Histogram plot
+# series.hist()
+# # Density plot
+# series.plot(kind="kde")
+
+
+# Box and whisker plots
+# FIXME:
+# Tive de fazer com outro dataset, porque o fornecido não traz a data como é esperado (o ano vem definido como ('1', '2', '3'))
+# Vou ter de fazer um groupby especifico para que o dataset shampoo-sales.csv funcione neste tipo de gráficos
+
+# pattern = re.compile("1-")
+# for key in series.keys():
+# 	if pattern.match(key):
+# 		print(key)
+
+groups = series_b.groupby(Grouper(freq='A'))
+years = DataFrame()
+for name, group in groups:
+    years[name.year] = group.values
+years.boxplot()
+
+
+# Heat maps
+
+
+pyplot.show()

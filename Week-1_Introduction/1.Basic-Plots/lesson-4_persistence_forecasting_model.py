f93e2dc162a2f8cedef9bd322551023fd9bb8a5e
Luis Marques <luisserafim99@gmail.com>
2020-06-02 18:35:28 +0100

    Lesson 4 update

--- Week-1_Introduction/1.Basic-Plots/lesson-4_persistence_forecasting_model.py
+++ Week-1_Introduction/1.Basic-Plots/lesson-4_persistence_forecasting_model.py
@@ -11,6 +11,33 @@ series = read_csv(file, header=0, index_col=0, parse_dates=[0], squeeze=True)
 
 # Create lagged dataset
 values = DataFrame(series.values)
-dataframe = concat([values.shift(1), values], axis=1)
-dataframe.columns = ["t-1", "t+1"]
-print(dataframe.head())
+data_frame = concat([values.shift(1), values], axis=1)
+data_frame.columns = ["t-1", "t+1"]
+print(data_frame.head())
+
+# Split into train and test sets
+X = data_frame.values
+train_size = int(len(X) * 0.66)
+train, test = X[1: train_size], X[train_size:]
+train_X, train_y = train[:, 0], train[:, 1]
+test_X, test_y = test[:, 0], test[:, 1]
+
+
+# Persistence model
+def model_persistence(x):
+    return x
+
+
+# Walk-forward validation
+predictions = list()
+for x in test_X:
+    yhat = model_persistence(x)
+    predictions.append(yhat)
+test_score = mean_squared_error(test_y, predictions)
+print(f"Test MSE: {format(test_score, '0.3f')}")
+
+# Plot predictions and expected results
+pyplot.plot(train_y)
+pyplot.plot([None for i in train_y] + [x for x in test_y])
+pyplot.plot([None for i in train_y] + [x for x in predictions])
+pyplot.show()

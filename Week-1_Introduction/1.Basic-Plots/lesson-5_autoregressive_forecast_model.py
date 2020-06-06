a9101f04bfe5007f1ac1c933888c823fb634d28c
Luis Marques <luisserafim99@gmail.com>
2020-06-06 04:49:25 +0100

    Lesson 5 update

--- Week-1_Introduction/1.Basic-Plots/lesson-5_autoregressive_forecast_model.py
+++ Week-1_Introduction/1.Basic-Plots/lesson-5_autoregressive_forecast_model.py
@@ -1,6 +1,9 @@
 import os
 from pandas import read_csv
+from pandas import DataFrame
+from pandas import concat
 from matplotlib import pyplot
+from pandas.plotting import lag_plot
 
 PATH = os.path.join(".", "Week-1_Introduction", "1.Basic-Plots")
 os.chdir(PATH)
@@ -8,6 +11,16 @@ os.chdir(PATH)
 series = read_csv(os.path.join(
     "files", "daily-minimum-temperatures.csv"), header=0, index_col=0)
 
-print(series.head())
-series.plot()
-pyplot.show()
+# print(series.head())
+
+# series.plot()
+# pyplot.show()
+
+# lag_plot(series)
+# pyplot.show()
+
+values = DataFrame(series.values)
+data_frame = concat([values.shift(1), values], axis=1)
+data_frame.columns = ["t-1", "t+1"]
+result = data_frame.corr()
+print(result)
